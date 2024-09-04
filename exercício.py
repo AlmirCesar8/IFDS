a08c7ecea38bdce3ead81ab9b9e2bcfc1974c8a0
Almircesar8 <cesar25silvafilho@gmail.com>
2024-09-04 09:58:53 -0300

    first commit

---
+++ exercício.py
@@ -0,0 +1,4 @@
+n1 = float(input("Insira sua primeira nota: "))
+n2 = float(input("Insira sua segunda nota: "))
+m = (n1 + n2)/2
+print("Sua média foi:", m)
---
+++ script.py
@@ -0,0 +1,5 @@
+mensagem1 = "Bom dia, estamos aprendendo:"
+mensagem2 = "Python"
+
+print(mensagem1)
+print(mensagem2)
